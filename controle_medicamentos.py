133b33a6561e0eec5f3f98c47473627c3fbb630c
marcosfcarvalho <43345062+marcosfcarvalho@users.noreply.github.com>
2025-05-18 02:42:33 -0300

    Add files via upload

---
+++ controle_medicamentos.py
@@ -0,0 +1,193 @@
+import sqlite3
+import os
+import datetime
+
+DB_PATH = 'controle_medicamentos.db'
+
+def conectar_banco():
+    conn = sqlite3.connect(DB_PATH)
+    return conn
+
+def criar_tabela():
+    conn = conectar_banco()
+    cursor = conn.cursor()
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS paciente (
+            id_paciente INTEGER PRIMARY KEY AUTOINCREMENT,
+            nome TEXT NOT NULL unique,
+            stts INTEGER CHECK(stts IN (0, 1)) NOT NULL DEFAULT 1, -- verifica se o prontuario esta ativo
+            observacao TEXT
+        )
+    ''')
+
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS medicamento (
+            id_medicamento INTEGER PRIMARY KEY AUTOINCREMENT,
+            nome TEXT NOT NULL,
+            descricao text
+        )
+    ''')
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS prescricao (
+            id_pres INTEGER PRIMARY KEY AUTOINCREMENT,
+            id_paciente INTEGER NOT NULL,
+            id_medicamento INTEGER NOT NULL,
+            dosagem_diaria INTEGER not null default 0,
+            Miligrama REAL not null,
+            observacao TEXT,
+            FOREIGN KEY (id_paciente) REFERENCES paciente(id_paciente) on delete cascade,
+            FOREIGN KEY (id_medicamento) REFERENCES medicamento(id_medicamento) on delete cascade
+        )
+    ''')
+
+    cursor.execute('''
+        CREATE TABLE if not exists estoque (
+            id_estoque INTEGER PRIMARY KEY AUTOINCREMENT,
+            id_medicamento INT NOT NULL,
+            quantidade_atual INT NOT NULL,
+            unidade_alerta INT NOT NULL,
+            FOREIGN KEY (id_medicamento) REFERENCES medicamento(id_medicamento) ON DELETE CASCADE
+);           
+                   
+                   ''')
+    conn.commit()
+    conn.close()
+
+def inserir_paciente(nome, observacao):
+    conn = conectar_banco()
+    cursor = conn.cursor()
+    try:
+        cursor.execute('''
+            INSERT INTO paciente (nome, observacao)
+            VALUES (?, ?)
+        ''', (nome, observacao))
+        conn.commit()
+        # Captura o ID gerado
+        id_paciente = cursor.lastrowid
+        print(f"Paciente inserido com sucesso! ID: {id_paciente}")
+        return id_paciente
+    except sqlite3.IntegrityError:
+        print("Paciente já cadastrado.")
+        return None
+    finally:
+        conn.close()
+
+def inserir_medicamento(nome, tipo, descricao):
+    conn = conectar_banco()
+    cursor = conn.cursor()
+    try:
+        cursor.execute('''
+            INSERT INTO medicamento (nome, tipo, descricao)
+            VALUES (?, ?, ?)
+        ''', (nome, tipo, descricao))
+        conn.commit()
+    except sqlite3.IntegrityError:
+        print("Medicamento já cadastrado.")
+    finally:
+        conn.close()
+
+def inserir_prescricao(id_paciente, id_medicamento, dosagem_fixa, dosagem_diaria, observacao):
+    conn = conectar_banco()
+    cursor = conn.cursor()
+    try:
+        cursor.execute('''
+            INSERT INTO prescricao (id_paciente, id_medicamento, dosagem_fixa, dosagem_diaria, observacao)
+            VALUES (?, ?, ?, ?, ?)
+        ''', (id_paciente, id_medicamento, dosagem_fixa, dosagem_diaria, observacao))
+        conn.commit()
+    except sqlite3.IntegrityError:
+        print("Prescrição já cadastrada.")
+    finally:
+        conn.close()
+
+
+def buscar_medicamento_por_nome(nome_medicamento):
+    conn = conectar_banco()
+    cursor = conn.cursor()
+    cursor.execute('''
+        SELECT id_medicamento FROM medicamento WHERE nome = ?
+    ''', (nome_medicamento,))
+    resultado = cursor.fetchone()
+    conn.close()
+    return resultado[0] if resultado else None
+
+def menuprincipal():
+    print ("""
+          1- Cadastrar novo paciente
+          2- Ajustar medicamentos
+          3- consultar medicamentos/pacientes
+          4- declarar alta/reativar prontuario
+          5- cadastrar medicamento
+          6- sair
+          """)
+
+criar_tabela()
+
+while True:
+    menuprincipal()
+    opcao = int(input("Escolha uma opção: "))
+
+    match opcao:
+        case 1:
+            nome = input("Digite o nome do paciente: ")
+            obs = input("Digite uma observação: ")
+            id_paciente = inserir_paciente(nome, obs)
+            nome_medicamento = insert ('Digite o nome do medicamento: ')
+            id_medicamento = buscar_medicamento_por_nome(nome_medicamento)
+
+
+        case 2:
+            id_paciente = int(input("Digite o ID do paciente: "))
+            id_medicamento = int(input("Digite o ID do medicamento: "))
+            dosagem_fixa = int(input("A dosagem é fixa? (1 para sim, 0 para não): "))
+            dosagem_diaria = int(input("Digite a dosagem diária: "))
+            observacao = input("Digite uma observação: ")
+            
+            
+            conn = conectar_banco()
+            cursor = conn.cursor()
+            cursor.execute('''
+                INSERT INTO prescricao (id_paciente, id_medicamento, dosagem_fixa, dosagem_diaria, observacao)
+                VALUES (?, ?, ?, ?, ?)
+            ''', (id_paciente, id_medicamento, dosagem_fixa, dosagem_diaria, observacao))
+            conn.commit()
+            conn.close()
+            print("Medicamento ajustado com sucesso.")
+
+        case 3:
+            conn = conectar_banco()
+            cursor = conn.cursor()
+            cursor.execute('''
+                SELECT id_paciente, nome, observacao FROM paciente WHERE stts = 1
+            ''')
+            pacientes = cursor.fetchall()
+            print("Pacientes cadastrados:\n\n")
+            for paciente in pacientes:
+                print(f"ID: {paciente[0]}, Nome: {paciente[1]}, Observação: {paciente[2]}")
+            conn.close()
+
+        case 4:
+            id_paciente = int(input("Digite o ID do paciente: "))
+            conn = conectar_banco()
+            cursor = conn.cursor()
+            cursor.execute('''
+                UPDATE paciente SET stts = 0 WHERE id_paciente = ?
+            ''', (id_paciente,))
+            conn.commit()
+            conn.close()
+            print("Paciente teve alta.")
+
+        case 5:
+            nome = input("Digite o nome do medicamento: ")
+            tipo = input("Digite o tipo do medicamento: ")
+            descricao = input("Digite uma descrição: ")
+            inserir_medicamento(nome, tipo, descricao)
+
+        
+        case 6:
+            print("Saindo do sistema...")
+            exit()
+            break
+        case _:
+            print("Opção inválida!")
+
